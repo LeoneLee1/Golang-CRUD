4066f446e2baaaca0dfa285fbd97bdb1067a0d60
laravel-webprofil <mawoloti@gmail.com>
2025-11-03 12:38:35 +0700

    routes protected group

--- routes/routes.go
+++ routes/routes.go
@@ -11,17 +11,25 @@ func UserRoutes(r *gin.Engine) {
 	userController := controllers.UserController{}
 	authController := controllers.AuthController{}
 
-	r.POST("/api/login", authController.Login)
+	auth := r.Group("/api")
 
-	user := r.Group("/api/users")
-	user.Use(middleware.AuthMiddleware())
-
+	// AUTH ROUTES
 	{
-		user.GET("", userController.GetUsers)
-		user.POST("/create", userController.CreateUsers)
-		user.GET("/:id", userController.GetUsersByID)
-		user.PUT("/update/:id", userController.UpdateUsers)
-		user.DELETE("/delete/:id",userController.DeleteUsers)
-		user.GET("/profile",userController.Profile)
+		auth.POST("/api/login", authController.Login)
+	}
+
+
+	protected := r.Group("/api")
+	protected.Use(middleware.AuthMiddleware())
+
+
+	// USER ROUTES
+	{
+		protected.GET("/users", userController.GetUsers)
+		protected.POST("/users/create", userController.CreateUsers)
+		protected.GET("/users/:id", userController.GetUsersByID)
+		protected.PUT("/users/update/:id", userController.UpdateUsers)
+		protected.DELETE("/users/delete/:id",userController.DeleteUsers)
+		protected.GET("/users/profile",userController.Profile)
 	}
 }
